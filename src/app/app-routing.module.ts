ec71f428e41e6b8a664dfe433fcd0f35fdf87f61
maruinesmendoza <53840283+maruinesmendoza@users.noreply.github.com>
2019-10-06 18:40:54 -0300

    Routing PAcientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PacientesComponent } from '../app/modules/pacientes/pacientes.component'
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'Pacientes', component: PacientesComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
