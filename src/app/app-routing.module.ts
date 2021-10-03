c06e25f4f3e153ef1304d3ffc9617a5eae9b71d1
maruinesmendoza <53840283+maruinesmendoza@users.noreply.github.com>
2021-10-03 17:12:50 -0300

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { PacientesComponent } from '../app/modules/pacientes/pacientes.component'
 import { ConsultasComponent } from '../app/modules/consultas/consultas.component'
 import { ApmComponent } from '../app/modules/apm/apm.component'
-import { ReporteComponent } from '../app/modules/reporte/reporte.component'
 import { GestionDeTurnosComponent }  from '../app/modules/gestion-de-turnos/gestion-de-turnos.component'
 import { PagosComponent }  from '../app/modules/pagos/pagos.component'
 import { ObraSocialComponent }  from '../app/modules/obra-social/obra-social.component'
@@ -14,7 +13,7 @@ const routes: Routes = [
   { path: 'Pacientes', component: PacientesComponent },
   { path: 'Consultas', component: ConsultasComponent },
   { path: 'APM', component: ApmComponent },
-  { path: 'Reportes', component: ReporteComponent },
+  
   { path: 'GestionTurnos', component: GestionDeTurnosComponent },
   { path: 'Pagos', component: PagosComponent },  
   { path: 'ObraSocial', component: ObraSocialComponent },
