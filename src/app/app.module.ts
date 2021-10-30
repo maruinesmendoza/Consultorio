7e428092f8c59c6fdfadbfb489a0b62658d7d75f
maruinesmendoza <53840283+maruinesmendoza@users.noreply.github.com>
2021-10-30 11:21:12 -0300

    git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { JwtInterceptor, ErrorInterceptor,fakeBackendProvider } from './helpers';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
@@ -13,6 +15,9 @@ import { GestionDeTurnosComponent } from './modules/gestion-de-turnos/gestion-de
 import { PagosComponent } from './modules/pagos/pagos.component';
 import { ObraSocialComponent } from './modules/obra-social/obra-social.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlertComponent } from './modules/alert/alert.component';
+import { LoginComponent } from './modules/login/login.component';
+import { MainComponent } from './modules/main/main.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
@@ -24,13 +29,12 @@ import {MatNativeDateModule } from '@angular/material/core';
 import {MatListModule} from '@angular/material/list';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import {OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 import {MatSelectModule} from '@angular/material/select'
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-import { LoginComponent } from './modules/login/login.component';
-import { MainComponent } from './modules/main/main.component';
-import { CommonModule } from '@angular/common';
 import {MatFormFieldModule} from '@angular/material/form-field';
+
+import {CommonModule } from '@angular/common';
 
 
 
@@ -46,7 +50,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     PagosComponent,
     ObraSocialComponent,
     LoginComponent,
-    MainComponent
+    MainComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
@@ -70,15 +75,13 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatSnackBarModule,  
     CommonModule,
     MatFormFieldModule,
-  
-
-
-    
-
-    
-
   ],
-  providers: [MatDatepickerModule,MatCheckboxModule],
+  providers: [MatDatepickerModule,MatCheckboxModule,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider],
   bootstrap: [AppComponent],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
